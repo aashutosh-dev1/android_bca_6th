9ab446833b83df161e38c0cabadddc6a344d5616
Aashutosh <aashutosh.poudel8@gmail.com>
2024-06-12 07:47:16 +0545

    implemented values strings, array and spinner with on item selected

--- app/src/main/java/com/example/layouts/MainActivity.java
+++ app/src/main/java/com/example/layouts/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.layouts;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -18,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     EditText username_editText, password_editText;
     Button blue_button;
 
+    Spinner random_spinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
         blue_button = findViewById(R.id.btn_submit);
 
+        random_spinner = findViewById(R.id.spn);
 
         blue_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,6 +40,18 @@ public class MainActivity extends AppCompatActivity {
                String typedUsername = String.valueOf(username_editText.getText());
                String typedPassowrd  = String.valueOf(password_editText.getText());
                 Toast.makeText(MainActivity.this, " Username =" +typedUsername +"\n"+" Password = " + typedPassowrd , Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        random_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Selected item  =" + parent.getSelectedItem(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
 
             }
         });
